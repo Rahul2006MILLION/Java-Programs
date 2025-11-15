750165b3dbf8852ad421f48ed712a04c5b3b5ef7
Rahul Jain <148176604+Rahul2006MILLION@users.noreply.github.com>
2025-11-15 21:15:50 +0530

    Add files via upload

---
+++ Marks.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Marks {
+    public static void main(String[] args) {
+        int marks = new Scanner(System.in).nextInt();
+        System.out.println("Marks: " + marks);
+    }
+}
+
