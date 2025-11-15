2d11cbcdc45109f6ca98cd523ffa5814094a0b9d
Rahul Jain <148176604+Rahul2006MILLION@users.noreply.github.com>
2025-11-15 21:14:08 +0530

    Add files via upload

---
+++ M2IfElse.java
@@ -0,0 +1,8 @@
+public class M2IfElse {
+    public static void main(String[] args) {
+        int n = 10;
+        if(n > 0) System.out.println("Positive");
+        else System.out.println("Negative");
+    }
+}
+
