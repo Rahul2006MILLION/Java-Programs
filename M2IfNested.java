6e9c2979a1bf450839e342856455cdcb722f4159
Rahul Jain <148176604+Rahul2006MILLION@users.noreply.github.com>
2025-11-15 21:14:46 +0530

    Add files via upload

---
+++ M2IfNested.java
@@ -0,0 +1,9 @@
+public class M2IfNested {
+    public static void main(String[] args) {
+        int n = 50;
+        if(n > 0){
+            if(n % 2 == 0) System.out.println("Positive Even");
+        }
+    }
+}
+
