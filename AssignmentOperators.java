ba9372699b931245bbd94a4d98ea67454a19a425
Rahul Jain <148176604+Rahul2006MILLION@users.noreply.github.com>
2025-12-02 22:09:38 +0530

    Add files via upload

---
+++ AssignmentOperators.java
@@ -0,0 +1,15 @@
+public class AssignmentOperators {
+    public static void main(String[] args) {
+        int a = 10;
+
+        a += 5;
+        System.out.println("After += 5 : " + a);
+
+        a -= 3;
+        System.out.println("After -= 3 : " + a);
+
+        a *= 2;
+        System.out.println("After *= 2 : " + a);
+    }
+}
+
