90b606f4a6030665c62aa2d8eaeabe6296e1257e
Rahul Jain <148176604+Rahul2006MILLION@users.noreply.github.com>
2025-12-02 22:20:05 +0530

    Add files via upload

---
+++ PaymentSystem.java
@@ -0,0 +1,26 @@
+class Payment {
+    void pay() { System.out.println("General payment"); }
+}
+
+class CreditCardPayment extends Payment {
+    void pay() { System.out.println("Paid using Credit Card"); }
+}
+
+class UPIPayment extends Payment {
+    void pay() { System.out.println("Paid using UPI"); }
+}
+
+class NetBankingPayment extends Payment {
+    void pay() { System.out.println("Paid using Net Banking"); }
+}
+
+public class PaymentSystem {
+    public static void main(String[] args) {
+        Payment p;
+
+        p = new CreditCardPayment();  p.pay();
+        p = new UPIPayment();         p.pay();
+        p = new NetBankingPayment();  p.pay();
+    }
+}
+
